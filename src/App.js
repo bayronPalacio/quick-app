440d50306c8eac6a00e3444f15d5961e34154d5d
bayronPalacio <58010308+bayronPalacio@users.noreply.github.com>
2021-02-15 09:13:19 -0800

    Added Registration Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import Login from './pages/Login';
+import Registration from './pages/Registrations';
+import {
+  BrowserRouter as Router,
+  Route, Switch,
+} from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-        <Login/>
+        <div>
+          <Route path="/login" component={Login} exact/>
+          <Route path="/registration" component={Registration}/>
+        </div>
     </div>
+    </Router>
   );
 }
 
