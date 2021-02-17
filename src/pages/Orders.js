a646afd4582353339891b15061b7c23c9918dc63
patricianar <patico832@hotmail.com>
2021-02-16 22:26:35 -0800

    navbar

---
+++ src/pages/Orders.js
@@ -0,0 +1,13 @@
+  
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            <h1>Orders</h1>
+            <p>List of orders placed here</p>
+        </div>
+    );
+}
+
+export default Orders;
