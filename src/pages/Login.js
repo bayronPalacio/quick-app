8349c397aeb71841f0d09283f9f0e53f7e24a44e
bayronPalacio <bapalacior@unal.edu.co>
2021-03-20 18:33:12 -0700

    Added cookie

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,40 +1,44 @@
 import Modal from "react-bootstrap/Modal";
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Cookies from "js-cookie";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const history = useHistory();
 
   const ValidateUser = async (e) => {
     e.preventDefault();
     const result = await fetch(`/login`, {
-      method: 'post',
+      method: "post",
       body: JSON.stringify({ email, password: email, password }),
       headers: {
-        'Content-Type': 'application/json',
-      }
+        "Content-Type": "application/json",
+      },
     });
     const response = await result.json();
     console.log(response);
-    if (response['responseServer'] === "yes") {
+    if (response["responseServer"]["Response"] === "Yes") {
+      Cookies.set("Company", response["responseServer"]["Company"]);
       history.push("/dashboard");
-      console.log('Successfully Login');
-    }
-    else {
+      console.log("Successfully Login");
+    } else {
       console.log("user no exist");
     }
-    setEmail('');
-    setPassword('');
-  }
+    setEmail("");
+    setPassword("");
+  };
 
   const ToRegistration = () => {
     history.push("/Registration");
-  }
+  };
   return (
     <>
-      <Modal show={true} dialogClassName={"primaryModal"} className="mx-auto"
+      <Modal
+        show={true}
+        dialogClassName={"primaryModal"}
+        className="mx-auto"
         size="md"
         centered
       >
@@ -45,25 +49,58 @@ const Login = () => {
               <form className="px-4 py-3">
                 <div className="form-group">
                   {/* <!-- <label for="exampleDropdownFormEmail1">Email address</label> --> */}
-                  <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} className="form-control" id="exampleDropdownFormEmail1" placeholder="email@service.com" />
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    className="form-control"
+                    id="exampleDropdownFormEmail1"
+                    placeholder="email@service.com"
+                  />
                 </div>
                 <div className="form-group">
                   {/* <!-- <label for="exampleDropdownFormPassword1">Password</label> --> */}
-                  <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} className="form-control" id="exampleDropdownFormPassword1" placeholder="Password" />
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    className="form-control"
+                    id="exampleDropdownFormPassword1"
+                    placeholder="Password"
+                  />
                 </div>
                 <div className="form-group h1-title">
                   <div className="form-check">
-                    <input type="checkbox" className="form-check-input" id="dropdownCheck" />
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="dropdownCheck"
+                    />
                     <label className="form-check-label" htmlFor="dropdownCheck">
                       Remember me
-                      </label>
+                    </label>
                   </div>
                 </div>
-                <button type="submit" onClick={ValidateUser} className="btn button-color btn-block">Sign in</button>
-                <p className="text-center font-italic font-weight-light">By clicking Sign In, you agree to our Terms.</p>
+                <button
+                  type="submit"
+                  onClick={ValidateUser}
+                  className="btn button-color btn-block"
+                >
+                  Sign in
+                </button>
+                <p className="text-center font-italic font-weight-light">
+                  By clicking Sign In, you agree to our Terms.
+                </p>
                 <div className="dropdown-divider"></div>
-                <a className="dropdown-item text-center font-italic" href="./Registration">No Acount? Sign up</a>
-                <a className="dropdown-item text-center font-italic" href="#">Forgot password?</a>
+                <a
+                  className="dropdown-item text-center font-italic"
+                  href="./Registration"
+                >
+                  No Acount? Sign up
+                </a>
+                <a className="dropdown-item text-center font-italic" href="#">
+                  Forgot password?
+                </a>
               </form>
             </div>
           </div>
@@ -71,7 +108,6 @@ const Login = () => {
       </Modal>
     </>
   );
-
-}
+};
 
 export default Login;
